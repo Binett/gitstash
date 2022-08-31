9d3cadb6c274faaadd7f00798d33f81a2c2e9f23
Binett <73723725+Binett@users.noreply.github.com>
2022-08-31 19:53:42 +0200

    Test

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
 import './App.css';
+import Test from './components/Test';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Test />
     </div>
   );
 }
